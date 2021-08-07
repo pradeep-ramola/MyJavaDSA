f57ac3fec05d1131d2da1a8b2cdcb4e438a9ff20
pradeep-ramola <54614006+pradeep-ramola@users.noreply.github.com>
2021-08-07 09:47:12 +0530

    Linked List from array

---
+++ LinkedListCreationUsingArray.java
@@ -0,0 +1,48 @@
+
+
+public class LinkedListCreationUsingArray {
+	  //  Node
+	static class Node  
+	{ 
+	    int data; 
+	    Node next; 
+	}
+	//head of LinkedList
+	static Node head;  
+	  
+	// Function to insert node 
+	static Node insert(Node head, int arrayElement) 
+	{ 
+	    Node newNode = new Node(); 
+	    newNode.data = arrayElement; 
+	    newNode.next = head; 
+	    head = newNode; 
+	    return head; 
+	} 
+	  
+	static void displayList(Node head) 
+	{ 
+	    while (head != null)  
+	    { 
+	        System.out.print(head.data + " "); 
+	        head = head.next; 
+	    } 
+	} 
+	  
+	static Node arrayToLinkedList(int arr[], int n) 
+	{ 
+	    head = null; 
+	    for (int i = 0; i < n ; i++) 
+	        head = insert(head, arr[i]); 
+	    return head; 
+	} 
+	  
+	 
+	public static void main(String[] args) 
+	{ 
+	    int arr[] = { 1, 2, 3, 6, 8, 9}; 
+	    int n = arr.length; 
+	    Node head = arrayToLinkedList(arr, n); 
+	    displayList(head); 
+	} 
+	} 
